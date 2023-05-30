6a30fc09aa74e368ac1c18766bd5933edfe6161e
Anna Khaustova <anna.a.khaustova@outlook.com>
2023-05-30 12:36:01 +0300

    traid-in is modified. animation is added

--- scripts/traid-in.js
+++ scripts/traid-in.js
@@ -1,7 +1,16 @@
 'use strict';
 
-//     window.addEventListener('scroll', function() {
-//         document.querySelectorAll('.changes').forEach(item => {
-//     console.log(item);
-//     })
-//   });
+
+function opacity() {
+    items.forEach(item => {
+        if(this.window.scrollY >= item.getBoundingClientRect().top) {
+            item.style.transition = 1.5 + 's';
+            item.style.opacity = 1;
+        }
+    });
+}
+
+let items = document.querySelectorAll('.changes__item_js');
+
+window.addEventListener('scroll',  opacity);
+
